29944aa4e260c2f5f00e95145318bafb08adabab
rienshaliu <rienshaliu@gmail.com>
2016-05-27 03:56:20 +0800

    add others material and meat class

---
+++ src/Client/Meat.java
@@ -0,0 +1,45 @@
+package Client;
+
+import java.io.File;
+import java.io.IOException;
+
+import processing.core.PImage;
+
+public class Meat {
+	private File[] meatList;
+	MainApplet parent;
+	String strMaterial;
+	private PImage image;
+	int x, y;
+	int width, height;
+	
+	public Meat(MainApplet parent) {
+		meatList = new File("material" + "/meat").listFiles();
+		this.parent = parent;
+		this.width = 100;
+		this.height = 75;
+		this.x = 500;
+		this.y = 100;
+	}
+	
+	public String getBread(int index) {
+		//TODO return what is in the burger
+		try {
+			strMaterial = meatList[index].getCanonicalPath();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return strMaterial;
+	}
+
+	public void setImage(PImage img) {
+		this.image = img;
+	}
+	
+	public void display() {
+		parent.noTint();
+		parent.image(image, x, y, width, height);
+	}
+}
---
+++ src/Client/OthersMaterial.java
@@ -0,0 +1,44 @@
+package Client;
+import java.io.File;
+import java.io.IOException;
+
+import processing.core.PImage;
+
+public class OthersMaterial {
+	private File[] othersList;
+	MainApplet parent;
+	String strMaterial;
+	private PImage image;
+	int x, y;
+	int width, height;
+	
+	public OthersMaterial(MainApplet parent) {
+		othersList = new File("material" + "/others").listFiles();
+		this.parent = parent;
+		this.width = 100;
+		this.height = 75;
+		this.x = 500;
+		this.y = 100;
+	}
+	
+	public String getBread(int index) {
+		//TODO return what is in the burger
+		try {
+			strMaterial = othersList[index].getCanonicalPath();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return strMaterial;
+	}
+
+	public void setImage(PImage img) {
+		this.image = img;
+	}
+	
+	public void display() {
+		parent.noTint();
+		parent.image(image, x, y, width, height);
+	}
+}
