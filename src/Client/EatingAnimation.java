7e3e1872d38cf48f0e29ebbc0bd6126d61efd22f
qqhsuanwu <hsuan@172.20.141.24>
2016-05-31 16:34:21 +0800

    update Country picture~ add eatingAnimation

--- src/Client/EatingAnimation.java
+++ src/Client/EatingAnimation.java
@@ -1,5 +1,48 @@
 package Client;
 
+import java.io.File;
+import java.io.IOException;
+
+import processing.core.PImage;
+
 public class EatingAnimation {
+	/*
+	 * EatingAnimation
+	 */
+	private File[] eatingAnimationList;
+	MainApplet parent;
+	String strMaterial;
+	private PImage image;
+	int x, y;
+	int width, height;
+	
+	public EatingAnimation(MainApplet parent) {
+		eatingAnimationList = new File("eating_animation").listFiles();
+		this.parent = parent;
+		this.width = 1000;
+		this.height = 600;
+		this.x = 0;
+		this.y = 0;
+	}
+	
+	public String getEating(int index) {
+		//TODO return what is in the burger
+		try {
+			strMaterial = eatingAnimationList[index].getCanonicalPath();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return strMaterial;
+	}
+
+	public void setImage(PImage img) {
+		this.image = img;
+	}
+	
+	public void display() {
+		parent.noTint();
+		parent.image(image, x, y, width, height);
+	}
 
 }
