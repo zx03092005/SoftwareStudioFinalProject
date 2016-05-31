eca8ebda50deae83b7e23d675359bc4e735a1acb
kittychen <kitty19960117@gmail.com>
2016-05-31 23:38:01 +0800

    change window size

--- src/Client/GameClient.java
+++ src/Client/GameClient.java
@@ -6,7 +6,7 @@ public class GameClient {
 	public static void main(String[] args) {
 		MyWindow window = new MyWindow();
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		window.setBounds(183, 45, 1000, 650);
+		window.setBounds(50, 0, 1200, 750);
 		window.setVisible(true);
 	}
 }
